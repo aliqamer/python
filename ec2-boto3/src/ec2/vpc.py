8d0a4867bd198dccc60a6756aa5272efcbde4a4e
aliqamer <qameralishaikh@gmail.com>
2019-11-03 23:21:21 +1100

    adding subnet, igt

--- ec2-boto3/src/ec2/vpc.py
+++ ec2-boto3/src/ec2/vpc.py
@@ -21,3 +21,48 @@ class VPC:
                 'Value': resource_name
             }]
         )
+
+    def create_internet_gateway(self):
+        print('Creating an Internet Gateway...')
+        return self._client.create_internet_gateway()
+
+    def attach_igw_to_vpc(self, vpc_id, igw_id):
+        print('Attaching Internet GW ' + igw_id + ' to VPC '+vpc_id)
+        return self._client.attach_internet_gateway(
+            InternetGatewayId=igw_id,
+            VpcId=vpc_id
+        )
+
+    def create_subnet(self, vpc_id, cidr_block):
+        print('Creating a subnet for VPC' +
+              vpc_id + ' with CIDR Block '+cidr_block)
+        return self._client.create_subnet(
+            VpcId=vpc_id,
+            CidrBlock=cidr_block
+        )
+
+    def create_public_route_table(self, vpc_id):
+        print('Creating public Route Table for VPC '+vpc_id)
+        return self._client.create_route_table(VpcId=vpc_id)
+
+    def create_igw_route_to_public_route_table(self, rtb_id, igw_id):
+        print('Adding route for IGW '+igw_id+' to Route Table '+rtb_id)
+        return self._client.create_route(
+            RouteTableId=rtb_id,
+            GatewayId=igw_id,
+            DestinationCidrBlock='0.0.0.0/0'
+        )
+
+    def associate_subnet_with_route_table(self, subnet_id, rtb_id):
+        print('Associating subnet '+subnet_id+' with Route Table '+rtb_id)
+        return self._client.associate_route_table(
+            SubnetId=subnet_id,
+            RouteTableId=rtb_id
+        )
+
+    def allow_auto_assign_ip_addresses_for_subnet(self, subnet_id):
+        print('Auto assigning ip address for subnet '+subnet_id)
+        return self._client.modify_subnet_attribute(
+            SubnetId=subnet_id,
+            MapPublicIpOnLaunch={'Value': True}
+        )
--- ec2-boto3/test_deployment.py
+++ ec2-boto3/test_deployment.py
@@ -18,6 +18,46 @@ def main():
 
     print('Added ' + vpc_name + ' to '+vpc_id)
 
+    # Create an IGW
+    igw_response = vpc.create_internet_gateway()
+
+    igw_id = igw_response['InternetGateway']['InternetGatewayId']
+
+    vpc.attach_igw_to_vpc(vpc_id, igw_id)
+
+    # Create a public subnet
+    public_subnet_respone = vpc.create_subnet(vpc_id, '10.0.1.0/24')
+
+    public_subnet_id = public_subnet_respone['Subnet']['SubnetId']
+
+    print('Subnet created for VPC '+vpc_id+':' + str(public_subnet_respone))
+
+    # Add name tag to Public Subnet
+    vpc.add_name_tag(public_subnet_id, 'Boto3-Public-Subnet')
+
+    # Create a public route table
+    public_route_table_response = vpc.create_public_route_table(vpc_id)
+
+    rtb_id = public_route_table_response['RouteTable']['RouteTableId']
+
+    # Adding IGW to public route table
+    vpc.create_igw_route_to_public_route_table(rtb_id, igw_id)
+
+    # Associate public subnet with Route Table
+    vpc.associate_subnet_with_route_table(public_subnet_id, rtb_id)
+
+    # Allow auto-assign public ip addresses for subnet
+    vpc.allow_auto_assign_ip_addresses_for_subnet(public_subnet_id)
+
+    # Create a Private Subnet
+    private_subnet_response = vpc.create_subnet(vpc_id, '10.0.2.0/24')
+    private_subnet_id = private_subnet_response['Subnet']['SubnetId']
+
+    print('Created private subnet '+private_subnet_id+' for VPC '+vpc_id)
+
+    # Add name tag to Private Subnet
+    vpc.add_name_tag(private_subnet_id, 'Boto3-Private-Subnet')
+
 
 if __name__ == '__main__':
     main()
